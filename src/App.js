e1c68919fb20d009eb42ec95a993e0cffd680bb5
Oskar Lindblad <lindbladoskar@gmail.com>
2021-11-29 21:51:11 +0100

    More characters + "reset css" added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Wrap } from "./stories/Wrap/Wrap";
+import { Text } from "./stories/Text/Text";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrap>
+        <Text text="–Test-" />
+      </Wrap>
     </div>
   );
 }
